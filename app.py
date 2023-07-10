81e31f7a7aba5c9cb2fa7b503d529814f77a3dd1
unknown <imlaleet@gmail.com>
2023-07-10 08:00:23 +0545

    nn_output

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import pandas as pd
 import pickle
 import numpy as np
-
+import tensorflow as tf
 # df = pickle.load(open('temp_df.pkl', 'rb'))
 
 df = pd.read_pickle('temp_df.pkl')
@@ -16,6 +16,16 @@ similarity_score = pd.read_pickle('similarity_scores.pkl')
 pv = pd.read_pickle('pv2.pkl')
 similarity_scores = pd.read_pickle('col_similarity_scores2.pkl')
 col_df = pd.read_pickle('col_Df2.pkl')
+
+
+#neural_network_pickle_files
+enumerate_bookids = pd.read_pickle('nn_pkl_files/enumerate_bookids.pkl')
+enumerate_userids = pd.read_pickle('nn_pkl_files/enumerate_userids.pkl')
+enumerate_bookids_reverse = pd.read_pickle('nn_pkl_files/enumerate_bookids_reverse.pkl')
+model = tf.keras.models.load_model('nn_pkl_files/model_3.h5')
+nn_books = pd.read_pickle('nn_pkl_files/nn_books.pkl')
+nn_books_2 = pd.read_pickle('nn_pkl_files/nn_books_2.pkl')
+nn_ratings = pd.read_pickle('nn_pkl_files/nn_ratings.pkl')
 
 app = Flask(__name__)
 
@@ -78,6 +88,48 @@ def col_recommend():
         books.append(items)
     return render_template('collaborative_recommendations.html', data=books)
 
+@app.route('/nn_recommendations')
+def nn_recommendations():
+    return render_template('neural_net_preds.html')
+
+
+def get_books_rated_by_user(user_id):
+  results=[]
+  user_ratings_data = nn_ratings[nn_ratings["user_id"]==user_id]
+ 
+  books_read_by_user = nn_books_2[nn_books_2["book_id"].isin(user_ratings_data.book_id.values)]
+  print(books_read_by_user)
+  for index, row in books_read_by_user.iterrows():
+    values = [row['title'], row["description"], row['image_url'], row['author_name'], row["genres"]]
+    results.append(values)
+  return results
+
+@app.route('/nn_recommend', methods=["post"])
+def recommend_book_nn():
+  user_id = request.form.get('id')
+  user_id = int(user_id)
+  results = []
+  books_readby_user = nn_ratings[nn_ratings.user_id == user_id]
+  
+  books_not_read = nn_books_2[~nn_books_2["book_id"].isin(books_readby_user.book_id.values)]["book_id"]
+  filtered_books_not_read = list(set(books_not_read).intersection(set(enumerate_bookids.keys())))
+  books_not_read_by_user = [[enumerate_bookids.get(x)] for x in filtered_books_not_read]
+  user_idx = enumerate_userids.get(user_id)
+  user_books = np.hstack(([[user_idx]] * len(books_not_read_by_user), books_not_read_by_user ))
+  ratings = model.predict([user_books[:, 0], user_books[:, 1]]).flatten()
+  rating_idxs = np.argsort(ratings)[-10:][::-1]
+  recommendations_idx = [enumerate_bookids_reverse.get(books_not_read_by_user[x][0]) for x in rating_idxs]
+
+  #get books rated by user
+  books_rated_by_user = get_books_rated_by_user(user_id)
+  print(books_rated_by_user)
+
+  books = nn_books_2[nn_books_2["book_id"].isin(recommendations_idx)]
+
+  for index, row in books.iterrows():
+    values = [row['title'], row["description"], row['image_url'], row['author_name'], row["genres"]]
+    results.append(values)
+  return render_template('neural_net_preds.html', data=results, user_books_data = books_rated_by_user)
 
 
 if __name__ == 'main':
